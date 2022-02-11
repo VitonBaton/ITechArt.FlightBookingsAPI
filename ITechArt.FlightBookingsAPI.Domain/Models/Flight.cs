eb8175e7e0ce1beaa129fcdf871eaea48695d5a3
VitonBaton <vitonbaton@yandex.ru>
2022-02-11 10:42:23 +0300

    Add database models

---
+++ ITechArt.FlightBookingsAPI.Domain/Models/Flight.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITechArt.FlightBookingsAPI.Domain.Models;
+
+public class Flight
+{
+    public Guid Id { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    [Required]
+    public DateTime EndTime { get; set; }
+    [Required]
+    public string? DeparturePoint { get; set; }
+    [Required]
+    public string? DestinationPoint { get; set; }
+    public List<Ticket>? Tickets { get; set; }
+    public List<FlightTicketType>? FlightTickets { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+}
---
+++ ITechArt.FlightBookingsAPI.Domain/Models/FlightTicketType.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITechArt.FlightBookingsAPI.Domain.Models;
+
+public class FlightTicketType
+{
+    public Guid Id { get; set; }
+    public Guid FlightId { get; set; }
+    public Flight? Flight { get; set; }
+    public Guid TicketTypeId { get; set; }
+    public TicketType? TicketType { get; set; }
+    [Column(TypeName = "decimal(5, 2)")]
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
+    public int TotalCount { get; set; }
+}
