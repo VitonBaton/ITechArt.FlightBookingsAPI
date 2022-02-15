da090358b3767faf7b56d4629161ccc0e9ddf87e
VitonBaton <vitonbaton@yandex.ru>
2022-02-15 20:18:07 +0300

    Add settings model

---
+++ ITechArt.FlightBookingsAPI.Web/Utils/JwtSettingsModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ITechArt.FlightBookingsAPI.Web.Utils;
+
+public class JwtSettingsModel
+{
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+}
