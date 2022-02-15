d82ebe92dda018c4eca4430f56bc90218ef2f806
VitonBaton <vitonbaton@yandex.ru>
2022-02-15 20:18:22 +0300

    Add users profile

---
+++ ITechArt.FlightBookingsAPI.Web/MappingConfigurations/UsersProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ITechArt.FlightBookingsAPI.Domain.Models;
+using ITechArt.FlightBookingsAPI.Web.Models;
+
+namespace ITechArt.FlightBookingsAPI.Web.MappingConfigurations;
+
+public class UsersProfile : Profile
+{
+    public UsersProfile()
+    {
+        CreateMap<RegistrationModel, User>();
+    }
+}
