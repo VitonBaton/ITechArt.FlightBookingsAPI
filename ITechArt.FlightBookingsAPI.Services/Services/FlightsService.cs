aa010e590ca01a54093ce982a5cec227806ea4f3
VitonBaton <vitonbaton@yandex.ru>
2022-03-01 09:58:59 +0300

    Add flight service realization

---
+++ ITechArt.FlightBookingsAPI.Services/Services/FlightsService.cs
@@ -0,0 +1,51 @@
+﻿using ITechArt.FlightBookingsAPI.Domain.Models;
+using ITechArt.FlightBookingsAPI.Infrastructure.Contexts;
+using ITechArt.FlightBookingsAPI.Infrastructure.Interfaces;
+using ITechArt.FlightBookingsAPI.Services.Interfaces;
+
+namespace ITechArt.FlightBookingsAPI.Services.Services;
+
+public class FlightsService : IFlightsService
+{
+    private IFlightsRepository _repository;
+
+    public FlightsService(IFlightsRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<IEnumerable<Flight>> GetAllAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Flight> GetByIdAsync(Guid id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task<Flight> CreateAsync(Flight flight)
+    {
+        return await _repository.CreateAsync(flight);
+    }
+
+    public async Task UpdateAsync(Guid id, Flight flight)
+    {
+        var updatable = await _repository.GetByIdAsync(id);
+
+        foreach (var property in flight.GetType().GetProperties())
+        {
+            var value = property.GetValue(flight);
+            if (!(value is null || value.Equals(DateTime.MinValue) || value.Equals(Guid.Empty)))
+            {
+                property.SetValue(updatable,value);
+            }
+        }
+        await _repository.UpdateAsync(updatable);
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _repository.DeleteAsync(id);
+    }
+}
