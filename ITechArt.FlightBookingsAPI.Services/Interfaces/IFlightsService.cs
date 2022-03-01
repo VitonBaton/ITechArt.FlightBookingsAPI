3e8fb75574517ab07c8ce6ba5be93e31d5655824
VitonBaton <vitonbaton@yandex.ru>
2022-03-01 09:19:31 +0300

    Created interface for managing flights

---
+++ ITechArt.FlightBookingsAPI.Services/Interfaces/IFlightsService.cs
@@ -0,0 +1,12 @@
+﻿using ITechArt.FlightBookingsAPI.Domain.Models;
+
+namespace ITechArt.FlightBookingsAPI.Services.Interfaces;
+
+public interface IFlightsService
+{
+    public Task<IEnumerable<Flight>> GetAllAsync();
+    public Task<Flight> GetByIdAsync(Guid id);
+    public Task<Flight> CreateAsync(Flight flight);
+    public Task UpdateAsync(Flight flight);
+    public Task DeleteAsync(Guid id);
+}
