a38e3261b93313ad2300bfbd0ddd5a176613b1b7
VitonBaton <vitonbaton@yandex.ru>
2022-03-22 19:44:51 +0300

    Created separated constants file

---
+++ ITechArt.FlightBookingsAPI.Services/Constants/MessageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ITechArt.FlightBookingsAPI.Services.Constants;
+
+public static class MessageConstants
+{
+    public const string LoginError = "Incorrect login/password";
+    public const string UserNotFoundError = "User not found";
+    public const string DeleteError = "Error while deleting";
+}
