9c92d27978c4c1cae5cb376dcc4c63b01617b8fb
VitonBaton <vitonbaton@yandex.ru>
2022-03-24 18:16:54 +0300

    fix nullable warnings and add EnsureCreated

--- ITechArt.FlightBookingsAPI.Repositories/Contexts/FlightBookingsContext.cs
+++ ITechArt.FlightBookingsAPI.Repositories/Contexts/FlightBookingsContext.cs
@@ -7,14 +7,15 @@ namespace ITechArt.FlightBookingsAPI.Infrastructure.Contexts;
 
 public class FlightBookingsContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
 {
-    public DbSet<Flight> Flights { get; set; }
-    public DbSet<Ticket> Tickets { get; set; }
-    public DbSet<TicketType> TicketTypes { get; set; }
-    public DbSet<FlightTicketType> FlightTicketTypes { get; set; }
+    public DbSet<Flight> Flights { get; set; } = null!;
+    public DbSet<Ticket> Tickets { get; set; } = null!;
+    public DbSet<TicketType> TicketTypes { get; set; } = null!;
+    public DbSet<FlightTicketType> FlightTicketTypes { get; set; } = null!;
 
     public FlightBookingsContext(DbContextOptions<FlightBookingsContext> options)
         : base(options)
     {
+        Database.EnsureCreated();
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
