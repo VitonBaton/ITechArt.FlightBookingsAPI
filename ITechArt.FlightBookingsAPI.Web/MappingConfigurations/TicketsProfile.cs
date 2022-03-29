151d426882e1c67fd14212dda016d1753942667f
VitonBaton <vitonbaton@yandex.ru>
2022-03-29 15:29:47 +0300

    Add controller for managing tickets

---
+++ ITechArt.FlightBookingsAPI.Web/MappingConfigurations/TicketsProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ITechArt.FlightBookingsAPI.Domain.Models;
+using ITechArt.FlightBookingsAPI.Web.ViewModels;
+
+namespace ITechArt.FlightBookingsAPI.Web.MappingConfigurations;
+
+public class TicketsProfile : Profile
+{
+    public TicketsProfile()
+    {
+        CreateMap<FlightTicketType, FlightTicketTypeViewModel>();
+        CreateMap<FlightTicketTypeViewModel, FlightTicketType>();
+    }
+}
---
+++ ITechArt.FlightBookingsAPI.Web/ViewModels/FlightTicketTypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ITechArt.FlightBookingsAPI.Web.ViewModels;
+
+public class FlightTicketTypeViewModel
+{
+    public Guid Id { get; set; }
+    public Guid FlightId { get; set; }
+    public Guid TicketTypeId { get; set; }
+    [Column(TypeName = "decimal(5, 2)")]
+    [Required]
+    public decimal Price { get; set; }
+    [Required]
+    public int TotalCount { get; set; }
+}
