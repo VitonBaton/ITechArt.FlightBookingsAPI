f7c647b7a404ef128115ac960fdfa0f7fc3cc20b
VitonBaton <vitonbaton@yandex.ru>
2022-04-01 16:35:40 +0300

    Added service for booking tickets

--- ITechArt.FlightBookingsAPI.Services/Interfaces/ITicketsService.cs
+++ ITechArt.FlightBookingsAPI.Services/Interfaces/ITicketsService.cs
@@ -5,4 +5,9 @@ namespace ITechArt.FlightBookingsAPI.Services.Interfaces;
 public interface ITicketsService
 {
     public Task<IEnumerable<IGrouping<Guid,Ticket>>> GetAllAsync(Guid userId);
+    public Task<Ticket> GetByIdAsync(Guid userId,Guid ticketId);
+    public Task<Ticket> CreateAsync(Guid userId, Ticket ticket);
+    public Task UpdateAsync(Guid userId,Ticket ticket);
+    public Task DeleteAsync(Guid userId,Guid ticketId);   
+    
 }
--- ITechArt.FlightBookingsAPI.Services/Services/TicketsService.cs
+++ ITechArt.FlightBookingsAPI.Services/Services/TicketsService.cs
@@ -1,5 +1,7 @@
-﻿using ITechArt.FlightBookingsAPI.Domain.Models;
+﻿using ITechArt.FlightBookingsAPI.Domain.Errors;
+using ITechArt.FlightBookingsAPI.Domain.Models;
 using ITechArt.FlightBookingsAPI.Infrastructure.Interfaces;
+using ITechArt.FlightBookingsAPI.Services.Constants;
 using ITechArt.FlightBookingsAPI.Services.Interfaces;
 
 namespace ITechArt.FlightBookingsAPI.Services.Services;
@@ -16,5 +18,46 @@ public class TicketsService : ITicketsService
     public async Task<IEnumerable<IGrouping<Guid, Ticket>>> GetAllAsync(Guid userId)
     {
         return await _repository.GetAllAsync(userId);
+    }
+
+    public async Task<Ticket> GetByIdAsync(Guid userId, Guid ticketId)
+    {
+        var ticket = await _repository.GetByIdAsync(ticketId);
+        if (ticket.UserId != userId)
+        {
+            throw new ArgumentException(MessageConstants.TicketNotOwnedByUser);
+        }
+
+        return ticket;
+    }
+
+    public async Task<Ticket> CreateAsync(Guid userId, Ticket ticket)
+    {
+        ticket.UserId = userId;
+        var result =await _repository.CreateAsync(ticket);
+        return result;
+    }
+
+    public async Task UpdateAsync(Guid userId, Ticket ticket)
+    {
+        var updatable = await _repository.GetByIdAsync(ticket.Id);
+        if (updatable.UserId != userId)
+        {
+            throw new ArgumentException(MessageConstants.TicketNotOwnedByUser);
+        }
+
+        ticket.UserId = userId;
+        await _repository.UpdateAsync(ticket);
+    }
+
+    public async Task DeleteAsync(Guid userId, Guid ticketId)
+    {
+        var ticket = await _repository.GetByIdAsync(ticketId);
+        if (ticket.UserId != userId)
+        {
+            throw new ArgumentException(MessageConstants.TicketNotOwnedByUser);
+        }
+
+        await _repository.DeleteAsync(ticketId);
     }
 }
